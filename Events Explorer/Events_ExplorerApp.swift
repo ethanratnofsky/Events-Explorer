0dd6b3eb1e92d5bd71df08b3c83d8bbe6d3d490f
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2023-05-01 22:58:11 -0500

    Initial Commit

---
+++ Events Explorer/Events_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Events_ExplorerApp.swift
+//  Events Explorer
+//
+//  Created by Ethan Ratnofsky on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Events_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
